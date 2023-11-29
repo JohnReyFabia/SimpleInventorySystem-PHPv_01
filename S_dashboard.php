bd949f118d04fbdd47f030699b7739dced9ad114
JohnReyFabia <201880452@psu.palawan.edu.ph>
2023-11-29 09:40:54 +0800

    update

--- S_dashboard.php
+++ S_dashboard.php
@@ -1,6 +1,34 @@
 <?php require_once 'php_action/db_connect.php' ?>
 <?php require_once 'includes/S_header.php' ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inventory List</title>
+    <style>
+        .item-container {
+            display: flex;
+            flex-wrap: wrap;
+            gap: 10px;
+        }
+
+        .item {
+            width: 24%; /* Adjusted to account for the gap */
+            padding: 10px;
+            box-sizing: border-box;
+            text-align: center;
+            border: 1px solid #ddd; /* Border added */
+        }
+
+        .item img {
+            max-width: 100%;
+            border: 1px solid #ddd; /* Border added */}
+    </style>
+</head>
+<body>
+
 <div class="row">
 	<div class="col-md-12">
 	
@@ -13,6 +41,29 @@
 				<div class="remove-messages"></div>
 
 				<div class="div-action pull pull-right" style="padding-bottom:20px;">
+				
+				<?php	
+		
+				// Fetch inventory data from the database
+				$sql = "SELECT product_id, product_name, product_image FROM product WHERE status = 1";
+				$result = $connect->query($sql);
+			
+// Display the inventory in a grid
+echo '<div class="item-container">';
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo '<div class="item">';
+		echo '<img src= assests\images\stock\ '. $row['product_image'] . '" alt="' . $row['product_name'] . '" style="max-width: 100%;">';
+        echo '<p>' . $row['product_name'] . '</p>';
+
+        echo '<button onclick="borrowItem(' . $row['product_id'] . ')"> Add to Borrowed Cart</button>';
+        echo '</div>';
+    }
+}  
+
+?>
+
+
     
 				</div> <!-- /div-action -->				
 				
